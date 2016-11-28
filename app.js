1d9ac9eb9170a60aa6e23743907bb2169b4ee175
maimous@gmail.com <stefaleon@gmail.com>
2016-11-28 19:35:00 +0200

    add JSON.stringify

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
 	url: 'http://maps.googleapis.com/maps/api/geocode/json?address=Champs-%C3%89lys%C3%A9es',
 	json: true
 }, (error, response, body) => {
-	console.log(body);
+	console.log(JSON.stringify(body, undefined, 2));
 });
