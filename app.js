a62d68fe62254a6dbddd091c0ec34d208a0e063e
maimous@gmail.com <stefaleon@gmail.com>
2016-11-29 02:06:23 +0200

    refactor to abstract geocode

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode')
 
 const argv = yargs
 	.options({
@@ -16,19 +17,6 @@ const argv = yargs
 
 console.log(argv);
 
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address);
 
-request({
-	url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json: true
-}, (error, response, body) => {
-	if (error) {
-		console.log('Unable to connect to Google servers.');
-	} else if (body.status === 'ZERO_RESULTS') {
-		console.log('Address not found.');
-	} else if (body.status === 'OK') {
-		console.log(`Address: ${body.results[0.].formatted_address}`);
-		console.log(`Latitude: ${body.results[0.].geometry.location.lat}`);
-		console.log(`Longitude: ${body.results[0.].geometry.location.lng}`);	
-	}	
-});
+
